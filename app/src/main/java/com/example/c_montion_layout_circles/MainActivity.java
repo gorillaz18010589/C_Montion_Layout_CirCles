95a8065210b88539f480909b126cb9c168d59a1a
hank <gorillaz1801058@gmail.com>
2021-11-29 22:45:43 +0800

    v1.MontionLayout簡單範例,搭配google文件,影片註解

---
+++ app/src/main/java/com/example/c_montion_layout_circles/MainActivity.java
@@ -0,0 +1,14 @@
+package com.example.c_montion_layout_circles;
+//https://docs.google.com/document/d/1wmcb0TT1TxPHr5zqa1Vq_g7_eZ72I1EO40vjhyj88_o/edit# 參考google文件
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
